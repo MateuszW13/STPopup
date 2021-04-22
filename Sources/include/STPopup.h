4e73c92384aa58dedbca28b77809fa302e8f1f4f
Mateusz Wagner <mwagner@corelogic.pl>
2021-04-22 12:50:11 +0200

    Update

--- Sources/include/STPopup.h
+++ Sources/include/STPopup.h
@@ -6,11 +6,11 @@
 //  Copyright (c) 2015 Sth4Me. All rights reserved.
 //
 
-#ifndef STPopup_STPopup_h
-#define STPopup_STPopup_h
 
 #import <STPopup/STPopupController.h>
 #import <STPopup/STPopupNavigationBar.h>
 #import <STPopup/UIViewController+STPopup.h>
-
-#endif
+#import <STPopup/UIResponder+STPopup.h>
+#import <STPopup/STPopupControllerTransitioningSlideVertical.h>
+#import <STPopup/STPopupControllerTransitioningFade.h>
+#import <STPopup/STPopupLeftBarItem.h>
