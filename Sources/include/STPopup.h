b27b0bdb32c5060d8b70544fa6d667884d2dbe27
Mateusz Wagner <mwagner@corelogic.pl>
2021-04-22 12:54:50 +0200

    Update

--- Sources/include/STPopup.h
+++ Sources/include/STPopup.h
@@ -6,11 +6,12 @@
 //  Copyright (c) 2015 Sth4Me. All rights reserved.
 //
 
-
+#if SWIFT_PACKAGE
+#import <STPopupController.h>
+#import <STPopupNavigationBar.h>
+#import <UIViewController+STPopup.h>
+#else
 #import <STPopup/STPopupController.h>
 #import <STPopup/STPopupNavigationBar.h>
 #import <STPopup/UIViewController+STPopup.h>
-#import <STPopup/UIResponder+STPopup.h>
-#import <STPopup/STPopupControllerTransitioningSlideVertical.h>
-#import <STPopup/STPopupControllerTransitioningFade.h>
-#import <STPopup/STPopupLeftBarItem.h>
+#endif
